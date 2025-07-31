829f3cac8aaf72fe19516c51b78629fdad0ab7ee
Brendan Berg <brendan@berg.industries>
2025-07-30 22:33:49 -0700

    Add Github OIDC provider module (#5)

---
+++ github_oidc_provider/aws_iam_openid_connect_provider.tf
@@ -0,0 +1,17 @@
+locals {
+  openid_url = "https://token.actions.githubusercontent.com/.well-known/openid-configuration"
+}
+
+data "external" "thumbprint" {
+  program = ["${path.module}/bin/thumbprint.sh", local.openid_url]
+}
+
+resource "aws_iam_openid_connect_provider" "default" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com",
+  ]
+
+  thumbprint_list = [data.external.thumbprint.result.thumbprint]
+}
---
+++ github_oidc_provider/aws_iam_role.tf
@@ -0,0 +1,42 @@
+data "aws_iam_policy_document" "default" {
+  for_each = var.iam_role_policy_map
+
+  statement {
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.default.id]
+    }
+
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:${each.key}:*"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values = [
+        "sts.amazonaws.com"
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role" "default" {
+  for_each = data.aws_iam_policy_document.default
+
+  name                 = replace("${each.key}-GithubOidcRolePolicy", "/", "-")
+  description          = "Deploy access for Github Actions on ${each.key}"
+  max_session_duration = 3600 # 1h
+  assume_role_policy   = each.value.json
+}
+
+resource "aws_iam_role_policy_attachment" "default" {
+  for_each = aws_iam_role.default
+
+  role       = each.value.name
+  policy_arn = var.iam_role_policy_map[each.key]
+}
