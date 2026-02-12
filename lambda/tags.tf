4e181da9fbd67b9058e08de5cd4f7d5fcdf5d106
Brendan Berg <brendan@berg.industries>
2026-02-11 18:11:34 -0800

    Add Route53 zone modules and bump version number (#8)
    * add route53 zone module and embiggen version number
    * fix policy naming issue, tag module name, outputs

--- lambda/tags.tf
+++ lambda/tags.tf
@@ -3,7 +3,7 @@ module "tags" {
   inherited_tags = var.inherited_tags
   additional_tags = {
     "starframe.stencil.name"           = "lambda"
-    "starframe.stencil.version"        = "0.1.2"
+    "starframe.stencil.version"        = "0.1.3"
     "starframe.stencil.repository_url" = "starframe-systems/tf-stencils.git"
   }
 }
---
+++ route53_zone/iam.policy.read-only.tf
@@ -0,0 +1,49 @@
+data "aws_iam_policy_document" "read-only" {
+  statement {
+    sid = "route53hostedzone"
+
+    actions = [
+      "route53:GetHostedZone",
+      "route53:ListResourceRecordSets",
+      "route53:ListTagsForResource",
+      "route53:ListTagsForResources",
+    ]
+
+    resources = [
+      var.created_by_route53_registrar ? data.aws_route53_zone.default[0].arn : aws_route53_zone.default[0].arn
+    ]
+  }
+
+  statement {
+    sid = "route53change"
+
+    actions = [
+      "route53:GetChange",
+    ]
+
+    resources = [
+      "arn:aws:route53:::change/*"
+    ]
+  }
+
+  statement {
+    sid = "route53listhostedzones"
+
+    actions = [
+      "route53:ListHostedZones"
+    ]
+
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "read-only" {
+  name        = "${local.prefix}Route53ReadOnlyAccess"
+  description = "Route53 cross-account access for ${local.prefix}"
+  policy      = data.aws_iam_policy_document.read-only.json
+}
+
+resource "aws_iam_role_policy_attachment" "read-only" {
+  role       = var.role_name
+  policy_arn = aws_iam_policy.read-only.arn
+}
