e782edb5ec62062dccea5d18dc3172062376a9b7
Brendan Berg <brendan@berg.industries>
2026-06-16 14:33:01 -0700

    revert key_schema block

--- dynamo_db/ddb_table.tf
+++ dynamo_db/ddb_table.tf
@@ -33,11 +33,8 @@ resource "aws_dynamodb_table" "this" {
   dynamic "global_secondary_index" {
     for_each = var.global_secondary_indexes
     content {
-      name = global_secondary_index.value.name
-      key_schema {
-        attribute_name = global_secondary_index.value.hash_key
-        key_type       = "HASH"
-      }
+      name               = global_secondary_index.value.name
+      hash_key           = global_secondary_index.value.hash_key
       projection_type    = global_secondary_index.value.projection_type
       non_key_attributes = global_secondary_index.value.non_key_attributes
       read_capacity      = global_secondary_index.value.read_capacity
